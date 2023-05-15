99f1a5be83f5fdbb73decf779d111807fc3c63ce
Andrey <tanto39@mail.ru>
2023-05-15 17:27:28 +0300

    sign up state

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC, FormEvent } from "react";
+import React, { useState, FC, FormEvent, ChangeEvent } from "react";
 import {
   Container,
   TextField,
@@ -16,13 +16,22 @@ import {
 } from "@mui/material";
 import { useAppDispatch } from "../../hooks/redux";
 import { setAuth } from "../../store/reducers/ActionCreators";
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { ILogin } from "../../models/Ilogin";
+import { useNavigate } from "react-router-dom";
 
 const Login: FC = () => {
-  const [login, setLogin] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [userData, setUserData] = useState<ILogin>({} as ILogin);
   const [loginError, setLoginError] = useState<boolean>(false);
   const [passwordError, setPasswordError] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const changeInput = async (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    key: string
+  ) => {
+    setUserData({ ...userData, [key]: event.target.value });
+  };
 
   const dispatch = useAppDispatch();
 
@@ -32,11 +41,11 @@ const Login: FC = () => {
     setLoginError(false);
     setPasswordError(false);
 
-    if (login == "") {
+    if (userData.username == "") {
       setLoginError(true);
       return;
     }
-    if (password == "") {
+    if (userData.password == "") {
       setPasswordError(true);
       return;
     }
@@ -54,77 +63,86 @@ const Login: FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Вход
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            onChange={(e) => setLogin(e.target.value)}
-            value={login}
-            error={loginError}
-            margin="normal"
-            required
-            fullWidth
-            id="login"
-            label="Логин"
-            name="login"
-            autoComplete="login"
-            autoFocus
-          />
-          <TextField
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            error={passwordError}
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Пароль"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Запомнить меня"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Вход
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
           >
-            Войти
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Забыли пароль?
-              </Link>
+            <TextField
+              onChange={(event) => changeInput(event, "username")}
+              value={userData.username}
+              error={loginError}
+              margin="normal"
+              required
+              fullWidth
+              id="login"
+              label="Логин"
+              name="login"
+              autoComplete="login"
+              autoFocus
+            />
+            <TextField
+              onChange={(event) => changeInput(event, "password")}
+              value={userData.password}
+              error={passwordError}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Пароль"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Запомнить меня"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Войти
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Забыли пароль?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link
+                  href="#"
+                  onClick={() => navigate("/signup")}
+                  variant="body2"
+                >
+                  Зарегистрироваться
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                Зарегистрироваться
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  </ThemeProvider>
+      </Container>
+    </ThemeProvider>
   );
 };
 
