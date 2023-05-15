0f6f61c173c843e854a95083025e1eaff67fb08a
Andrey <tanto39@mail.ru>
2023-05-15 17:27:28 +0300

    sign up state

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,27 +1,40 @@
-import React, {FC, FormEvent} from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { ChangeEvent, FC, FormEvent, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ISignUp } from "../../models/ISignUp";
+import Login from "./Login";
+import { useNavigate } from "react-router-dom";
 
 const SignUp: FC = () => {
+  const [userData, setUserData] = useState<ISignUp>({} as ISignUp);
+  const navigate = useNavigate();
+
+  const changeInput = async (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    key: string
+  ) => {
+    setUserData({ ...userData, [key]: event.target.value });
+  };
+
   const theme = createTheme();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -32,18 +45,23 @@ const SignUp: FC = () => {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Зарегистрироваться
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -54,6 +72,8 @@ const SignUp: FC = () => {
                   id="firstName"
                   label="Имя"
                   autoFocus
+                  value={userData.firstname}
+                  onChange={(event) => changeInput(event, "firstname")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -64,6 +84,8 @@ const SignUp: FC = () => {
                   label="Фамилия"
                   name="lastName"
                   autoComplete="family-name"
+                  value={userData.lastname}
+                  onChange={(event) => changeInput(event, "lastname")}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -74,6 +96,8 @@ const SignUp: FC = () => {
                   label="Электронная почта"
                   name="email"
                   autoComplete="email"
+                  value={userData.email}
+                  onChange={(event) => changeInput(event, "email")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -85,6 +109,8 @@ const SignUp: FC = () => {
                   type="text"
                   id="username"
                   autoComplete="new-username"
+                  value={userData.username}
+                  onChange={(event) => changeInput(event, "username")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -96,6 +122,8 @@ const SignUp: FC = () => {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={userData.password}
+                  onChange={(event) => changeInput(event, "password")}
                 />
               </Grid>
             </Grid>
@@ -109,7 +137,11 @@ const SignUp: FC = () => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/login" variant="body2">
+                <Link
+                  href="#"
+                  variant="body2"
+                  onClick={() => navigate("/login")}
+                >
                   Уже есть аккаунт? Войти.
                 </Link>
               </Grid>
@@ -119,6 +151,6 @@ const SignUp: FC = () => {
       </Container>
     </ThemeProvider>
   );
-}
+};
 
 export default SignUp;
